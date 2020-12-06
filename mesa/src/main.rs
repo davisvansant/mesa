c042fd7a90bee14951ba61a9bbde75a831240c41
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-12-05 16:07:56 -0800

    initial layout for mesa

--- mesa/src/main.rs
+++ mesa/src/main.rs
@@ -1,3 +1,33 @@
+use clap::{crate_name, crate_version, App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let build = SubCommand::with_name("build")
+        .about("build and create your mesa")
+        .help("build and create your mesa");
+
+    let view = SubCommand::with_name("view")
+        .about("view (test) your mesa")
+        .help("view (test) your mesa");
+
+    let erode = SubCommand::with_name("erode")
+        .about("cleanup and remove your mesa")
+        .help("cleanup and remove your mesa");
+
+    let form = SubCommand::with_name("form")
+        .about("form (finalize and release) your mesa")
+        .help("form (finalize and release) your mesa");
+
+    let mesa = App::new(crate_name!())
+        .version(crate_version!())
+        .about("| mesa - an isolated place")
+        .subcommands(vec![build, view, erode, form])
+        .get_matches();
+
+    match mesa.subcommand_name() {
+        Some("build") => println!("build!"),
+        Some("view") => println!("view!"),
+        Some("erode") => println!("erode!"),
+        Some("form") => println!("form!"),
+        _ => println!("{}", mesa.usage()),
+    }
 }
