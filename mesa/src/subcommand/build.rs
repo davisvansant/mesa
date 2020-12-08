1c1a8ed0b0dfca2ade8e7d94c73ffcc474f53c46
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-12-08 14:49:30 -0800

    initial use of mesa_strata for cli

--- mesa/src/subcommand/build.rs
+++ mesa/src/subcommand/build.rs
@@ -1,3 +1,7 @@
 pub fn mesa_build() {
-    println!("build!");
+    let build = mesa_strata::docker_local::DockerLocal::build();
+    match build {
+        Ok(output) => println!("{}", String::from_utf8(output.stdout).unwrap()),
+        Err(error) => println!("{}", error),
+    };
 }
