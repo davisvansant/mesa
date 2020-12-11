f9610dce6e87b263b7bc4080cbabb6e74bed50b6
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-12-10 17:25:07 -0800

    initial addition of build cmd reading from plan

--- mesa/src/subcommand/build.rs
+++ mesa/src/subcommand/build.rs
@@ -1,5 +1,10 @@
+use crate::plan::MesaPlan;
+use std::fs::read;
+
 pub async fn mesa_build() {
-    let build = mesa_strata::docker_local::DockerLocal::build();
+    let file = read("./target/test.toml").unwrap();
+    let plan: MesaPlan = toml::from_slice(&file).unwrap();
+    let build = mesa_strata::docker_local::DockerLocal::build(plan.name);
     match build {
         Ok(output) => println!("{}", String::from_utf8(output.stdout).unwrap()),
         Err(error) => println!("{}", error),
