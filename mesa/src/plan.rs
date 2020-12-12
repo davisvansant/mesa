084d333a45d5e3f317cb85fc9000c97d2f5e83d7
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-12-12 11:29:31 -0800

    initial language field for plan toml

--- mesa/src/plan.rs
+++ mesa/src/plan.rs
@@ -5,6 +5,7 @@ use std::fs::write;
 pub struct MesaPlan {
     pub name: String,
     pub version: String,
+    pub language: Language,
 }
 
 impl MesaPlan {
@@ -12,11 +13,26 @@ impl MesaPlan {
         let plan: MesaPlan = MesaPlan {
             name: String::from("default_mesa_plan_name"),
             version: String::from("default_mesa_plan_version"),
+            language: Language {
+                name: SupportedLanguage::Rust,
+                version: String::from("1.48.0"),
+            },
         };
         let toml = toml::to_string(&plan).unwrap();
         write("./target/test.toml", toml).unwrap();
         println!("toml created");
     }
+}
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+pub struct Language {
+    name: SupportedLanguage,
+    version: String,
+}
+
+#[derive(Debug, Deserialize, PartialEq, Serialize)]
+pub enum SupportedLanguage {
+    Rust,
 }
 
 #[cfg(test)]
@@ -29,10 +45,16 @@ mod tests {
             r#"
             name = "test_mesa_plan"
             version = "0.1.0"
+
+            [language]
+            name = "Rust"
+            version = "1.48.0"
             "#,
         )
         .unwrap();
         assert_eq!(test_mesa_plan.name, String::from("test_mesa_plan"));
         assert_eq!(test_mesa_plan.version, String::from("0.1.0"));
+        assert_eq!(test_mesa_plan.language.name, SupportedLanguage::Rust);
+        assert_eq!(test_mesa_plan.language.version, String::from("1.48.0"));
     }
 }
