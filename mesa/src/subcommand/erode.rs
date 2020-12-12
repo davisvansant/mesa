f81f4133ba34d82422164942415410ccaf9843c4
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-12-12 12:25:31 -0800

    adjust container name

--- mesa/src/subcommand/erode.rs
+++ mesa/src/subcommand/erode.rs
@@ -1,4 +1,4 @@
 pub async fn mesa_erode() {
-    let container = String::from("tester");
+    let container = String::from("mesa_rust_1.48.0");
     mesa_strata::docker_local::DockerLocal::erode(container).await;
 }
