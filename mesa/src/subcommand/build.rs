f7b7cea8a7a10ba8d335f6643f0f6b87debb3713
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-12-12 12:25:57 -0800

    adjust build command to work with new build impl

--- mesa/src/subcommand/build.rs
+++ mesa/src/subcommand/build.rs
@@ -4,9 +4,12 @@ use std::fs::read;
 pub async fn mesa_build() {
     let file = read("./target/test.toml").unwrap();
     let plan: MesaPlan = toml::from_slice(&file).unwrap();
-    let build = mesa_strata::docker_local::DockerLocal::build(plan.name);
-    match build {
-        Ok(output) => println!("{}", String::from_utf8(output.stdout).unwrap()),
-        Err(error) => println!("{}", error),
-    };
+    println!("{:?}", plan.language.name);
+    println!("{:?}", plan.language.version);
+    // let build = mesa_strata::docker_local::DockerLocal::build(plan.name);
+    // match build {
+    //     Ok(output) => println!("{}", String::from_utf8(output.stdout).unwrap()),
+    //     Err(error) => println!("{}", error),
+    // };
+    mesa_strata::docker_local::DockerLocal::build(plan.name).await;
 }
