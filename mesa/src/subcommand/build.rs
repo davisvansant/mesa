6395dc73f8f227f897f8a58f0e3e2475c053b906
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-12-17 12:18:35 -0800

    pass in plan version for tagging

--- mesa/src/subcommand/build.rs
+++ mesa/src/subcommand/build.rs
@@ -4,6 +4,7 @@ use std::fs::read;
 pub async fn mesa_build() {
     let file = read("./target/test.toml").unwrap();
     let plan: MesaPlan = toml::from_slice(&file).unwrap();
+    // let tag: MesaPlan = toml::from_slice(&file).unwrap();
     println!("{:?}", plan.language.name);
     println!("{:?}", plan.language.version);
     // let build = mesa_strata::docker_local::DockerLocal::build(plan.name);
@@ -11,5 +12,5 @@ pub async fn mesa_build() {
     //     Ok(output) => println!("{}", String::from_utf8(output.stdout).unwrap()),
     //     Err(error) => println!("{}", error),
     // };
-    mesa_strata::docker_local::DockerLocal::build(plan.name).await;
+    mesa_strata::docker_local::DockerLocal::build(plan.name, plan.version).await;
 }
