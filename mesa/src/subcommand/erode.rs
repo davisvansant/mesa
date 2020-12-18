903b4e4393d4e85ca5c483d7a733a7fe0178980a
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-12-18 15:36:05 -0800

    adjust erode and view to pull fields from toml

--- mesa/src/subcommand/erode.rs
+++ mesa/src/subcommand/erode.rs
@@ -1,4 +1,10 @@
+use crate::plan::MesaPlan;
+use std::fs::read;
+
 pub async fn mesa_erode() {
-    let container = String::from("mesa_rust_1.48.0");
-    mesa_strata::docker_local::DockerLocal::erode(container).await;
+    let file = read("./target/test.toml").unwrap();
+    let plan: MesaPlan = toml::from_slice(&file).unwrap();
+    // let container = String::from("mesa_rust_1.48.0");
+    // mesa_strata::docker_local::DockerLocal::erode(container).await;
+    mesa_strata::docker_local::DockerLocal::erode(plan.name, plan.version).await;
 }
--- mesa/src/subcommand/view.rs
+++ mesa/src/subcommand/view.rs
@@ -1,4 +1,10 @@
+use crate::plan::MesaPlan;
+use std::fs::read;
+
 pub async fn mesa_view() {
-    let container = String::from("amazon/aws-lambda-provided:al2");
-    mesa_strata::docker_local::DockerLocal::view(container).await;
+    let file = read("./target/test.toml").unwrap();
+    let plan: MesaPlan = toml::from_slice(&file).unwrap();
+    // let container = String::from("amazon/aws-lambda-provided:al2");
+    // mesa_strata::docker_local::DockerLocal::view(container).await;
+    mesa_strata::docker_local::DockerLocal::view(plan.name, plan.version).await;
 }
