c21e87da6dbeaf18700e2a40ebf0c75b2f360477
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-12-21 17:50:36 -0800

    adjust build subcommand to accept new strings

--- mesa/src/subcommand/build.rs
+++ mesa/src/subcommand/build.rs
@@ -4,5 +4,11 @@ use std::fs::read;
 pub async fn mesa_build() {
     let file = read("./target/test.toml").unwrap();
     let plan: MesaPlan = toml::from_slice(&file).unwrap();
-    mesa_strata::docker_local::DockerLocal::build(plan.name, plan.version).await;
+    mesa_strata::docker_local::DockerLocal::build(
+        plan.name,
+        plan.version,
+        plan.language.version,
+        plan.formation.layer,
+    )
+    .await;
 }
