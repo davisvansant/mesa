bfb1c46db91a48fc727071989ddd4b6c515b9e6f
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-12-23 12:56:23 -0800

    adjust plan to create/find toml in root directory, update tests

--- mesa/src/plan.rs
+++ mesa/src/plan.rs
@@ -26,12 +26,12 @@ impl MesaPlan {
             },
         };
         let toml = toml::to_string(&plan).unwrap();
-        write("./target/test.toml", toml).unwrap();
+        write("test.toml", toml).unwrap();
         println!("toml created");
     }
 
     pub async fn excavate() -> Result<MesaPlan, Box<dyn Error>> {
-        let file = read("./target/test.toml")?;
+        let file = read("test.toml")?;
         let plan: MesaPlan = toml::from_slice(&file)?;
         Ok(plan)
     }
@@ -63,26 +63,48 @@ pub enum FormationShape {
 mod tests {
     use super::*;
 
-    #[test]
-    fn plan() {
-        let test_mesa_plan: MesaPlan = toml::from_str(
-            r#"
-            name = "test_mesa_plan"
-            version = "0.1.0"
+    // #[test]
+    // fn plan() {
+    //     let test_mesa_plan: MesaPlan = toml::from_str(
+    //         r#"
+    //         name = "test_mesa_plan"
+    //         version = "0.1.0"
+    //
+    //         [language]
+    //         name = "Rust"
+    //         version = "1.48.0"
+    //
+    //         [formation]
+    //         shape = "Lambda"
+    //         layer = "amazon/aws-lambda-provided:al2"
+    //         "#,
+    //     )
+    //     .unwrap();
+    //     assert_eq!(test_mesa_plan.name, String::from("test_mesa_plan"));
+    //     assert_eq!(test_mesa_plan.version, String::from("0.1.0"));
+    //     assert_eq!(test_mesa_plan.language.name, SupportedLanguage::Rust);
+    //     assert_eq!(test_mesa_plan.language.version, String::from("1.48.0"));
+    // }
 
-            [language]
-            name = "Rust"
-            version = "1.48.0"
+    #[tokio::test]
+    async fn init() {
+        MesaPlan::init().await;
+        let open_test_mesa_plan = std::fs::File::open("test.toml").unwrap();
+        let metadata = open_test_mesa_plan.metadata().unwrap();
+        assert_eq!(metadata.is_file(), true);
+    }
 
-            [formation]
-            shape = "Lambda"
-            layer = "amazon/aws-lambda-provided:al2"
-            "#,
-        )
-        .unwrap();
-        assert_eq!(test_mesa_plan.name, String::from("test_mesa_plan"));
+    #[tokio::test]
+    async fn excavate() {
+        let test_mesa_plan = MesaPlan::excavate().await.unwrap();
+        assert_eq!(test_mesa_plan.name, String::from("default_mesa_plan_name"));
         assert_eq!(test_mesa_plan.version, String::from("0.1.0"));
         assert_eq!(test_mesa_plan.language.name, SupportedLanguage::Rust);
         assert_eq!(test_mesa_plan.language.version, String::from("1.48.0"));
+        assert_eq!(test_mesa_plan.formation.shape, FormationShape::Lambda);
+        assert_eq!(
+            test_mesa_plan.formation.layer,
+            String::from("amazon/aws-lambda-provided:al2")
+        );
     }
 }
