5fe56d9767e136f46dc824d26cc3a5ef9e1d7762
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-12-31 14:48:00 -0800

    remove test to pass new clippy lints

--- mesa_strata/src/lib.rs
+++ mesa_strata/src/lib.rs
@@ -1,9 +1,9 @@
 pub mod docker_local;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
