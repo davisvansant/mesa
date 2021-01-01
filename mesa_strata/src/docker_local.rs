e13d25ad60bad935fcb38726f842f689bfbd8176
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-12-31 16:16:54 -0800

    update build and template data with ignore_tests flag

--- mesa_strata/src/docker_local.rs
+++ mesa_strata/src/docker_local.rs
@@ -80,16 +80,40 @@ impl DockerLocal {
         builder.push(':');
         builder.push_str(&builder_version);
 
-        let handlebars_data = json! ({
-            "builder": builder,
-            "cmd_one": "rustc --version",
-            "cmd_two": "rustup component add rustfmt",
-            "cmd_three": "rustup component add clippy",
-            "cmd_four": "rustfmt --version",
-            "cmd_five": "cargo clippy --version",
-            "test_one": "cargo fmt -- --check",
-            "formation": formation,
-        });
+        // for now, plan to pass in flag if set from cli
+        let ignore_tests = true;
+
+        let handlebars_data = match ignore_tests {
+            false => json! ({
+                "ignore_tests": false,
+                "builder": builder,
+                "cmd_one": "rustc --version",
+                "cmd_two": "rustup component add rustfmt",
+                "cmd_three": "rustup component add clippy",
+                "cmd_four": "rustfmt --version",
+                "cmd_five": "cargo clippy --version",
+                "test_one": "cargo fmt -- --check",
+                "formation": formation,
+            }),
+
+            true => json! ({
+                "ignore_tests": true,
+                "builder": builder,
+                "cmd_one": "rustc --version",
+                "formation": formation,
+            }),
+        };
+
+        // let handlebars_data = json! ({
+        //     "builder": builder,
+        //     "cmd_one": "rustc --version",
+        //     "cmd_two": "rustup component add rustfmt",
+        //     "cmd_three": "rustup component add clippy",
+        //     "cmd_four": "rustfmt --version",
+        //     "cmd_five": "cargo clippy --version",
+        //     "test_one": "cargo fmt -- --check",
+        //     "formation": formation,
+        // });
 
         let mut dockerfile = File::create(dockerfile_path)?;
 
