515fa4321954f059595abae0d41d4d4d284b95db
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-01-06 15:21:53 -0800

    update examples with working code, but panics on usage

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use lambda::{handler_fn, Context};
+use serde_json::Value;
+
+pub type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda::run(handler_fn(mesa_handler)).await?;
+
+    Ok(())
+}
+
+async fn mesa_handler(event: Value, _: Context) -> Result<Value, Error> {
+    println!("{:#?}", event);
+    Ok(event)
 }
