af150ab0d3184a3be8dad9633de8dcf387987247
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-01-14 14:54:53 -0800

    cleanup if statement

--- mesa/src/subcommand/build.rs
+++ mesa/src/subcommand/build.rs
@@ -1,20 +1,11 @@
 pub async fn mesa_build(ignore_tests: bool) -> Result<(), Box<dyn std::error::Error>> {
     let plan = mesa::plan::MesaPlan::excavate().await;
 
-    if ignore_tests {
-        match plan {
-            Ok(plan_details) => {
-                mesa::strata::docker_local::DockerLocal::build(plan_details, true).await?
-            }
-            Err(error) => println!("mesa build | unable to read plan : {}", error),
+    match plan {
+        Ok(plan_details) => {
+            mesa::strata::docker_local::DockerLocal::build(plan_details, ignore_tests).await?
         }
-    } else {
-        match plan {
-            Ok(plan_details) => {
-                mesa::strata::docker_local::DockerLocal::build(plan_details, false).await?
-            }
-            Err(error) => println!("mesa build | unable to read plan : {}", error),
-        }
+        Err(error) => println!("mesa build | unable to read plan : {}", error),
     }
 
     Ok(())
